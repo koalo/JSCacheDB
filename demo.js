a2572d84b69f16f3dacc3c410897bc0c8d26789c
Florian Meier <koalo@koalo.de>
2012-01-08 15:30:35 +0100

    Demo

---
+++ demo.js
@@ -0,0 +1,51 @@
+var database = new JSCacheDB("todoDatabase");
+
+function init() {
+  database.onrefresh = displayTodos;
+  database.onerror = function(message,context) {
+    alert("Error: "+message);
+  }
+  database.open("1.17",{"todo":["ID","done"]});
+}
+
+window.addEventListener("DOMContentLoaded", init, false);
+
+function addTodo() {
+  var todo = {};
+  todo.task = document.getElementById("task").value;
+  todo.done = false;
+  database.save("todo",todo);
+  document.getElementById("task").value = "";
+}
+
+function markTodo(id) {
+  database.get("todo", id, function(obj) {
+    obj.done = true;
+    database.save("todo",obj);
+  });
+}
+
+function displayTodos() {
+  database.getAll("todo", function(result) {
+    var todoList = document.createElement("ul");
+    for(row in result) {
+      var e = result[row];
+      var li = document.createElement("li");
+      var a = document.createElement("a");
+      var t = document.createTextNode(e.ID + " " + e.task + " " + e.done);
+
+      a.href = "javascript:markTodo("+e.ID+")";
+
+      a.appendChild(t);
+      li.appendChild(a);
+      todoList.appendChild(li);
+    }
+
+    var todoItems = document.getElementById("todoItems");
+    todoItems.innerHTML = todoList.innerHTML;
+  });
+
+  database.getAll("reserved_insertion_ranges", function(result) {
+    //alert(JSON.stringify(result));
+  });
+}
